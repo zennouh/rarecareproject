188ecb8422cf57559362be03ea8f924e406a7dab
oelaimar <oussama.el.aimar@gmail.com>
2026-03-11 21:15:58 +0000

    feat: add migration

---
+++ services/treatment/api/database/migrations/2026_03_11_140025_create_treatment_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('treatment', function (Blueprint $table) {
+            $table->id();
+            $table->integer('patient_id'); // Link to Patient Service
+            $table->string('medication_name');
+            $table->string('dosage');
+            $table->string('frequency');
+            $table->date('start_date');
+            $table->date('end_date')->nullable();
+            $table->enum('status', ['active', 'completed', 'suspended'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('treatment');
+    }
+};
