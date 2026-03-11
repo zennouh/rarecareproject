9745e43a0e22f93ce231e174776c70c9c1d34c48
Yassmine Jabal <jabalyassmine2007@gmail.com>
2026-03-11 13:07:04 +0100

    re structer the project

--- services/disease/public/index.php
+++ services/disease/public/index.php
@@ -1,7 +1,28 @@
 <?php
-header('Content-Type: application/json');
-echo json_encode([
-    'service' => 'maladie',
-    'status' => 'ok',
-    'message' => 'Welcome to Maladie Service'
-]);
+
+/*
+|--------------------------------------------------------------------------
+| Create The Application
+|--------------------------------------------------------------------------
+|
+| First we need to get an application instance. This creates an instance
+| of the application / container and bootstraps the application so it
+| is ready to receive HTTP / Console requests from the environment.
+|
+*/
+
+$app = require __DIR__.'/../bootstrap/app.php';
+
+/*
+|--------------------------------------------------------------------------
+| Run The Application
+|--------------------------------------------------------------------------
+|
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
+|
+*/
+
+$app->run();
--- services/patients/public/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-header('Content-Type: application/json');
-echo json_encode([
-    'service' => 'patients',
-    'status' => 'ok',
-    'message' => 'Welcome to Patients Service'
-]);
