6ecbaf9c30643fe4b3a365177280022002931875
Abdellah-Moukarrame <moukarrameabdellah4@gmail.com>
2026-03-12 02:47:18 +0100

    add the logic of the crud

--- services/medical-record/api/app/Http/Controllers/MedicalRecordController.php
+++ services/medical-record/api/app/Http/Controllers/MedicalRecordController.php
@@ -11,28 +11,33 @@ class MedicalRecordController extends Controller
     public function index()
     {
         $record_medical = MedicalRecord::all();
-        return $record_medical;
+        // dd($record_medical);
+        if ($record_medical->isEmpty()) {
+            return response()->json(['message' => 'maintenant il ya pas aucun dossier medical :/'], 404);
+        } else {
+            return response()->json($record_medical, 200);
+        }
     }
     public function show($id)
     {
         $record_medical = MedicalRecord::find($id);
-        return $record_medical;
+        return response()->json($record_medical, 200);
     }
     public function create(Request $request)
     {
-        $record_medical = MedicalRecord::create($request->all());
-        return response()->json(['message' => $record_medical . 'Dossier Medical a etait creer avec succes :}', 200]);
+        MedicalRecord::create($request->all());
+        return response()->json(['message' =>  'Dossier Medical a etait creer avec succes :}'], 200);
     }
     public function update(Request $request, $id)
     {
         $record_medical = MedicalRecord::findOrFail($id);
         $record_medical->update($request->all());
-        return response()->json(['message' => 'Le Dossier Medical : ' . $record_medical->name . ' a etait bien Modifier ', 200]);
+        return response()->json(['message' => 'Le Dossier Medical : ' . $record_medical->name . ' a etait bien Modifier '], 200);
     }
     public function destroy($id)
     {
         $record_medical = MedicalRecord::findOrFail($id);
         $record_medical->delete();
-        return response()->json(['message' => 'Le dossier medicale' . $record_medical->name . 'a bien supprime 3iiiiich :µ', 200]);
+        return response()->json(['message' => 'Le dossier medicale' . $record_medical->name . 'a bien supprime 3iiiiich :µ'], 200);
     }
 }
