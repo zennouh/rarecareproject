4c5058bd8fada2b891ba5e6e7386bf4cc2afb991
nouh <nouh.nowile@gmail.com>
2026-03-13 09:25:04 +0000

    edit the name of class

--- services/patient/src/MessageHandler/PatientHandler.php
+++ services/patient/src/MessageHandler/PatientHandler.php
@@ -3,12 +3,13 @@
 namespace App\MessageHandler;
 
 use App\Message\Patient;
+use App\Message\PatientCreate;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
 final class PatientHandler
 {
-    public function __invoke(Patient $message)
+    public function __invoke(PatientCreate $message)
     {
         dump("Patient created: " . $message->patientId);
 
